f831a18d8a52af8fec77e4a24dac86b0e4fa0260
JEFFRIX BRIOL <91646993+abriol30@users.noreply.github.com>
2022-03-09 19:04:33 +0800

    Update control.php

--- data/control.php
+++ data/control.php
@@ -2,4 +2,20 @@
     session_start();
     require "connection.php";
 
+    $backgroundQuery = "SELECT * FROM teamsbackground where backgroundId = 1";
+    $background = mysqli_query($connect, $backgroundQuery);
+    $fetchBackground = mysqli_fetch_assoc($background);
+
+    $colorQuery = "SELECT * FROM themecolor where colorId = 1";
+    $color = mysqli_query($connect, $colorQuery);
+    $fetchColor = mysqli_fetch_assoc($color);
+
+    $logoQuery = "SELECT * FROM teamslogo where logoId = 1";
+    $logo = mysqli_query($connect, $logoQuery);
+    $fetchLogo = mysqli_fetch_assoc($logo);
+
+    $posterQuery = "SELECT * FROM teamsposter where posterId = 1";
+    $poster = mysqli_query($connect, $posterQuery);
+    $fetchPoster = mysqli_fetch_assoc($poster);
+
 ?>
