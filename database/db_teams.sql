3fb09f6eea77cc921f7c18d26d2316c20664147b
JEFFRIX BRIOL <91646993+abriol30@users.noreply.github.com>
2022-03-09 15:42:53 +0800

    Update db_teams.sql

--- database/db_teams.sql
+++ database/db_teams.sql
@@ -1,1 +1,14 @@
-create 
+create table themecolor(
+	colorId int primary key,
+    color_1 VARCHAR(7),
+    color_2 VARCHAR(7),
+    color_3 VARCHAR(7),
+    color_4 VARCHAR(7),
+    color_5 VARCHAR(7),
+    color_6 VARCHAR(7),
+	color_7 VARCHAR(7),
+	color_8 VARCHAR(7),
+	color_9 VARCHAR(7)
+);
+insert into themecolor(colorId,color_1,color_2,color_3,color_4,color_5,color_6,color_7,color_8,color_9)
+values(1,"#88C1FF","#97C7FB", "#AED5FF", "#FF6048","#FD8978","#4A63FF","#7788F4","#E5E5E5","#F8FAFF");
