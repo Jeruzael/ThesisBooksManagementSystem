4e7ec3156e173d44cd94c4a5d8a6a1bdd5bae7ba
JEFFRIX BRIOL <91646993+abriol30@users.noreply.github.com>
2022-03-09 19:04:26 +0800

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -13,15 +13,14 @@
         
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
-        <link rel="icon" type="png" href="../resources/logo2.png"/>
+        <link rel="icon" type="png" href="../resources/<?php echo $fetchLogo['logo_4']?>"/>
         <link rel="stylesheet" type="text/css" href="design.css"/>
         
     </head>
-    <body>
-        <div class="sidebar">
+    <body style="background-color: <?php echo $fetchColor['color_9']?>">
+        <div class="sidebar" style="background-color: <?php echo $fetchColor['color_7']?>">
             <div class="logo-details">
-              <img src="../resources/logo3.png" class="img-fluid" style="height: 50px;">
-                <div class="logo_name">Teams</div>
+              <img src="../resources/<?php echo $fetchLogo['logo_1']?>" class="img-fluid" style="height: 60px;">
                 <i class='bx bx-menu' id="btn" ></i>
             </div>
             <ul class="nav-list">
