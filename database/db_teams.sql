f2f4d22a8c133819dc681a2e4d8f9a8bfd0136f9
JEFFRIX BRIOL <91646993+abriol30@users.noreply.github.com>
2022-03-09 16:25:58 +0800

    ohmikoto

--- database/db_teams.sql
+++ database/db_teams.sql
@@ -1,4 +1,4 @@
-create table themecolor(
+create table themeColor(
 	colorId int primary key,
     color_1 VARCHAR(7),
     color_2 VARCHAR(7),
@@ -12,3 +12,13 @@ create table themecolor(
 );
 insert into themecolor(colorId,color_1,color_2,color_3,color_4,color_5,color_6,color_7,color_8,color_9)
 values(1,"#88C1FF","#97C7FB", "#AED5FF", "#FF6048","#FD8978","#4A63FF","#7788F4","#E5E5E5","#F8FAFF");
+
+create table teamsLogo(
+	logoId int primary key,
+    logo_1 VARCHAR(20),
+    logo_2 VARCHAR(20),
+    logo_3 VARCHAR(20),
+    logo_4 VARCHAR(20)
+);
+insert into teamsLogo(logoId,logo_1,logo_2,logo_3,logo_4)
+values(1,"teamsLogo_1.png","teamsLogo_2.png", "teamsLogo_3.png", "teamsLogo_4.png");
