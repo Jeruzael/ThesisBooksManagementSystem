ff2cb59af9fbc59297c7e3f912657a517c92e494
JEFFRIX BRIOL <91646993+abriol30@users.noreply.github.com>
2022-03-10 08:49:59 +0800

    Update db_teams.sql

--- database/db_teams.sql
+++ database/db_teams.sql
@@ -1,5 +1,5 @@
 create table themeColor(
-	colorId int primary key,
+	colorId int primary key auto_increment,
     color_1 VARCHAR(7),
     color_2 VARCHAR(7),
     color_3 VARCHAR(7),
@@ -10,34 +10,49 @@ create table themeColor(
 	color_8 VARCHAR(7),
 	color_9 VARCHAR(7)
 );
-insert into themecolor(colorId,color_1,color_2,color_3,color_4,color_5,color_6,color_7,color_8,color_9)
-values(1,"#88C1FF","#97C7FB", "#AED5FF", "#FF6048","#FD8978","#4A63FF","#7788F4","#E5E5E5","#F8FAFF");
+insert into themecolor(color_1,color_2,color_3,color_4,color_5,color_6,color_7,color_8,color_9)
+values("#88C1FF","#97C7FB", "#AED5FF", "#FF6048","#FD8978","#4A63FF","#7788F4","#E5E5E5","#F8FAFF");
 
 create table teamsLogo(
-	logoId int primary key,
+	logoId int primary key auto_increment,
     logo_1 VARCHAR(20),
     logo_2 VARCHAR(20),
     logo_3 VARCHAR(20),
     logo_4 VARCHAR(20)
 );
-insert into teamsLogo(logoId,logo_1,logo_2,logo_3,logo_4)
-values(1,"teamsLogo_1.png","teamsLogo_2.png", "teamsLogo_3.png", "teamsLogo_4.png");
+insert into teamsLogo(logo_1,logo_2,logo_3,logo_4)
+values("teamsLogo_1.png","teamsLogo_2.png", "teamsLogo_3.png", "teamsLogo_4.png");
 
 create table teamsPoster(
-	posterId int primary key,
+	posterId int primary key auto_increment,
     poster_1 VARCHAR(20),
     poster_2 VARCHAR(20),
     poster_3 VARCHAR(20),
     poster_4 VARCHAR(20),
     poster_5 VARCHAR(20)
 );
-insert into teamsPoster(posterId,poster_1,poster_2,poster_3,poster_4,poster_5)
-values(1,"poster_1.png","poster_2.png", "poster_3.png", "poster_4.png","poster_5.png");
+insert into teamsPoster(poster_1,poster_2,poster_3,poster_4,poster_5)
+values("poster_1.png","poster_2.png", "poster_3.png", "poster_4.png","poster_5.png");
 
 create table teamsBackground(
-	backgroundId int primary key,
+	backgroundId int primary key auto_increment,
     background_1 VARCHAR(50),
     background_2 VARCHAR(50)
 );
-insert into teamsBackground(backgroundId,background_1,background_2)
-values(1,"rgba(119, 136, 244, 0.21)","rgba(174, 213, 255, 0.53)");
+insert into teamsBackground(background_1,background_2)
+values("rgba(119, 136, 244, 0.21)","rgba(174, 213, 255, 0.53)");
+
+create table thesisLibrary(
+	bookId int primary key auto_increment,
+    bookCover varchar(100) default "bookdefaultcover.png",
+    bookTitle varchar(255) not null,
+    bookAuthor varchar(255) not null,
+    bookDescription varchar(255),
+    bookPublished date not null,
+    bookStatus varchar(50) default "available",
+    bookStamp datetime default current_timestamp
+);
+insert into thesisLibrary(bookTitle,bookAuthor,bookDescription,bookPublished)
+values
+("cyber-physical security of a chemical plant","prakash rao dunaka","the increasing number of cyber attacks on industries demands immediate attention for providing more secure mechanisms to safeguard industries and minimize
+risks.","2017-06-29");
