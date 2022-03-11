38f5d9758df006f88ba0deb16f8ee6834091609c
JEFFRIX BRIOL <91646993+abriol30@users.noreply.github.com>
2022-03-11 12:51:44 +0800

    Update index.php

--- user/index.php
+++ user/index.php
@@ -30,33 +30,42 @@
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
         <link href='https://unpkg.com/boxicons@2.0.7/css/boxicons.min.css' rel='stylesheet'>
         <link rel="icon" type="png" href="../resources/<?php echo $fetchLogo['logo_4']?>"/>
+        <link rel="stylesheet" type="text/css" href="design.css"/>
     </head>
-    <body>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light" style="padding:20px;">
+    <body style="background-color: <?php echo $fetchColor['color_9']?>;">
+        <nav class="navbar navbar-expand-lg navbar-light bg-light" style="padding:40px; background-color: <?php echo $fetchColor['color_9']?> !important;">
             <div class="container-fluid">
-                <img class="img-fluid" style="height:50px;" src="../resources/<?php echo $fetchLogo['logo_1']?>"/>
+                <img class="img-fluid" style="height:50px;margin-left:40px;" src="../resources/<?php echo $fetchLogo['logo_1']?>"/>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item" style="margin-left:30px;">
+                        <li class="nav-item" style="margin-left:40px;">
                             <a class="nav-link active" aria-current="page" href="#">Home</a>
                         </li>
-                        <li class="nav-item" style="margin-left:30px;">
+                        <li class="nav-item" style="margin-left:40px;">
                             <a class="nav-link" href="#">About us</a>
                         </li>
-                        <li class="nav-item" style="margin-left:30px;">
+                        <li class="nav-item" style="margin-left:40px;">
                             <a class="nav-link" href="#">Support</a>
                         </li>
                     </ul>
-                    <div class="d-flex">
-                        <button style="color: #000; background-color: <?php echo $fetchColor['color_9']?>;border-radius: 12px;padding: 10px;border: 1px solid <?php echo $fetchColor['color_9']?>;">Sign in</button>
-                        <button style="color: #fff; background-color: <?php echo $fetchColor['color_5']?>;border-radius: 12px;padding: 10px;border: 1px solid <?php echo $fetchColor['color_5']?>;">Sign up</button>
-                    </div>
+                    <form class="d-flex">
+                        <button class="form-control me-2" style="background-color: <?php echo $fetchColor['color_9']?>;border: 1px solid <?php echo $fetchColor['color_9']?>;padding:15px;">Sign in </button>
+                        <button class="btn" style="width:200px;background-color: <?php echo $fetchColor['color_5']?>;border: 1px solid <?php echo $fetchColor['color_5']?>;padding:15px;color:#fff;border-radius: 12px;">Sing up</button>
+                    </form>
                 </div>
             </div>
         </nav>
+        <div class="container-fluid row">
+            <div class="col-md" style="padding:10%;">
+                <div class="search__container"><input class="search__input" type="text" placeholder="Search Thesis Books..."></div>
+                <p style="font-weight: 700;font-size: 50px;line-height: 59px;">Thesis <label style="color: <?php echo $fetchColor['color_7']?>;">Archiving</label> System</p>
+                <p style="font-weight: 400;font-size: 30px;line-height: 45px;">An interactive books management system for the Computer Studies Department.</p>
+            </div>
+            <div class="col-md-6"><img class="img-fluid" src="../resources/<?php echo $fetchPoster['poster_1']?>"/></div>
+        </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
 </html>
