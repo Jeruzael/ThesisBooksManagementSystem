acecf3f7d5fb214b4d22792688f8d5cc250c0a86
JEFFRIX BRIOL <91646993+abriol30@users.noreply.github.com>
2022-03-11 13:42:32 +0800

    Update index.php

--- user/index.php
+++ user/index.php
@@ -66,6 +66,34 @@
             </div>
             <div class="col-md-6"><img class="img-fluid" src="../resources/<?php echo $fetchPoster['poster_1']?>"/></div>
         </div>
+        <div class="container-fluid" style="font-weight: 700;font-size: 45px;line-height: 53px;text-align: center; padding:10%;">Thesis Archiving System is the new home for your learning needs from the thesis books
+        <p style="font-weight: 400;font-size: 30px;line-height: 45px; margin-top:50px;">Connect, enable learning, and share knowledge</p>
+        <button style="border-radius: 12px;background-color:<?php echo $fetchColor['color_5']?>;border: 1px solid <?php echo $fetchColor['color_5']?>;font-weight: 400;font-size: 20px;line-height: 35px;padding:20px;color:#fff;width:300px;margin-top:50px;">Borrow books</button>
+        </div>
+        <div class="container-fluid row" style="background-color: <?php echo $fetchColor['color_7']?>;color:#fff;padding:0px;margin:0px;">
+        <div class="col-md" style="padding:5%;">
+            <img class="img-fluid" src="../resources/<?php echo $fetchLogo['logo_5']?>" style="height: 100px;"/>
+            <p>Thesis Archiving System is the new home for your learning needsfrom the thesis books</p>
+            <a href="#" style="text-decoration:none;color:#fff;">Learn more<i class='bx bx-right-arrow-alt'></i></a>
+        </div>
+        <div class="col-md" style="padding:5%; text-align:center">
+            <p style="font-weight: 700;font-size: 24px;line-height: 28px;">CONNECT WITH US</p>
+            <div class="row">
+                <div class="col" style="padding:10px;"><i class='bx bxl-facebook-circle' style="font-size:40px;"></i></div>
+                <div class="col" style="padding:10px;"><i class='bx bxl-instagram' style="font-size:40px;"></i></div>
+                <div class="col" style="padding:10px;"><i class='bx bxl-twitter' style="font-size:40px;"></i></div>
+            </div>
+        </div>
+        <div class="col-md" style="padding:5%;">
+            <p style="font-weight: 700;font-size: 24px;line-height: 28px;">CONTACT</p>
+            <div class="row">
+            <div class="col-1"><i class='bx bx-map' style="font-size:30px;"></i></div><div class="col"><p style="font-weight: 400;font-size: 15px;line-height: 18px;">Congressional Rd Ext, Barangay 171 Caloocan, Metro Manila</p></div></div>
+            <div class="row">
+            <div class="col-1"><i class='bx bx-phone' style="font-size:30px;"></i></div><div class="col"><p style="font-weight: 400;font-size: 15px;line-height: 18px;">+639123243423</p></div></div>
+            <div class="row">
+            <div class="col-1"><i class='bx bx-envelope' style="font-size:30px;"></i></div><div class="col"><p style="font-weight: 400;font-size: 15px;line-height: 18px;">teams@gmail.com</p></div></div>
+        </div>
+        </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
 </html>
