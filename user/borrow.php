6444d7a1feba0bc03fec299345e05318fc6e58a6
JEFFRIX BRIOL <91646993+abriol30@users.noreply.github.com>
2022-03-23 08:33:57 +0800

    Update borrow.php

--- user/borrow.php
+++ user/borrow.php
@@ -120,4 +120,31 @@
         </div>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
     </body>
+    <script src="pagination.js"></script>
+          <script>
+          let sidebar = document.querySelector(".sidebar");
+          let closeBtn = document.querySelector("#btn");
+          let searchBtn = document.querySelector(".bx-search");
+          let showbtn = document.querySelector("#feat-btn");
+          let bookshow = document.querySelector(".book-show");
+        
+          closeBtn.addEventListener("click", ()=>{
+            sidebar.classList.toggle("open");
+            menuBtnChange();//calling the function(optional)
+          });
+
+          showbtn.addEventListener("click", ()=>{
+            bookshow.classList.toggle("show");
+             
+           });
+        
+          // following are the code to change sidebar button(optional)
+          function menuBtnChange() {
+           if(sidebar.classList.contains("open")){
+             closeBtn.classList.replace("bx-menu", "bx-menu-alt-right");//replacing the iocns class
+           }else {
+             closeBtn.classList.replace("bx-menu-alt-right","bx-menu");//replacing the iocns class
+           }
+          }
+          </script>
 </html>
